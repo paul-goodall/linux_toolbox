1a49d2da9113fbbf27edbe9c810fdb524c07f9ff
paul-goodall <paul.thomas.goodall@gmail.com>
2020-09-26 11:47:27 +1000

    making the app source the repo

--- super_dash/app.R
+++ super_dash/app.R
@@ -1,44 +1,17 @@
 ## app.R ##
-library(shinydashboard)
 
-ui <- dashboardPage(
-    
-    dashboardHeader(title = "Goodsy Super-dash"),
-    
-    
-    ## Sidebar content
-    dashboardSidebar(
-        tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
-        ),
-        sidebarMenu(
-            menuItem("My Apps",     tabName = "my_apps",     icon = icon("app-store")),
-            menuItem("GLE",         tabName = "gle",         icon = icon("paint-brush")),
-            menuItem("ImageMagick", tabName = "imagemagick", icon = icon("hat-wizard")),
-            menuItem("FFMPEG",      tabName = "ffmpeg",      icon = icon("file-video")),
-            menuItem("Linux Tools", tabName = "linux_tools", icon = icon("linux"))
-        )
-    ),
-    ## Body content
-    dashboardBody(
-        tabItems(
-            tabItem(tabName = "my_apps",     source("tabs/my_apps.R",     local=TRUE)$value),
-            tabItem(tabName = "gle",         source("tabs/gle.R",         local=TRUE)$value),
-            tabItem(tabName = "imagemagick", source("tabs/imagemagick.R", local=TRUE)$value),
-            tabItem(tabName = "ffmpeg",      source("tabs/ffmpeg.R",      local=TRUE)$value),
-            tabItem(tabName = "linux_tools", source("tabs/linux_tools.R", local=TRUE)$value)
-        )
-    )
-)
-
-server <- function(input, output) {
-    set.seed(122)
-    histdata <- rnorm(500)
-    
-    output$plot1 <- renderPlot({
-        data <- histdata[seq_len(input$slider)]
-        hist(data)
-    })
+# ====================================================
+# Use the latest code from the repo
+my_base_path <-"https://raw.githubusercontent.com/paul-goodall/linux_toolbox/master/super_dash/"
+my_css <- "https://raw.githubusercontent.com/paul-goodall/linux_toolbox/master/super_dash/www/styles.css"
+if(as.character(Sys.info()["nodename"]) %like% "GoldEgwene"){
+    my_base_path <- ""
+    my_css <- "styles.css"
 }
+sauce <- function(my_file, base_path=my_base_path){
+    source(paste0(base_path, my_file), local=TRUE)$value
+}
+# ====================================================
 
-shinyApp(ui, server)
+
+sauce("app_content.R")
